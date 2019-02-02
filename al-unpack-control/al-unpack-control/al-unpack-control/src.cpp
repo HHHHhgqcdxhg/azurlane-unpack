82aedc40fdf9f50f2d062c8d0f9495f8bbd12822
HHHHhg <2894700792@qq.com>
2019-02-02 21:51:37 +0800

    unpack.exe

---
+++ al-unpack-control/al-unpack-control/al-unpack-control/src.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+#include <direct.h> 
+using namespace std;
+
+int main(int argc, char * argv[])
+{	
+	string cwd = _getcwd(NULL, 0);
+	string cmd = "\\Python37-32\\python.exe main";
+	cmd = cwd + cmd;
+	for (int i = 1; i < argc; i++)
+	{
+		cmd += " ";
+		cmd += argv[i];
+	}
+	//printf(cwd);
+	const char *cmd_c_str = cmd.c_str();
+	cout << cmd_c_str<<endl;
+	system(cmd_c_str);
+	return 0;
+}
