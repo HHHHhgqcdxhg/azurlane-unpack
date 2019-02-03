00e4dcc476c9b393165d554f9be1707276f706bf
HHHHhg <2894700792@qq.com>
2019-02-03 12:56:50 +0800

    quality

--- azurlaneUnpackControl.py
+++ azurlaneUnpackControl.py
@@ -4,21 +4,22 @@ from argparse import ArgumentParser
 from azurlaneUnpack import Azurlane_unpack
 
 CWD = os.path.dirname(os.path.abspath(__file__))
-azurlaneUnpack = Azurlane_unpack()
+
 class AzurlaneUnpackControl:
     def __init__(self,args):
         self.parse_args(args)
+        self.azurlaneUnpack = Azurlane_unpack(QUALITY=self.args.quality)
 
     def run(self):
         if self.args.file:
-            ret = azurlaneUnpack.unpackTexture(self.args.file)
+            ret = self.azurlaneUnpack.unpackTexture(self.args.file)
             if ret:
                 oPath,_ = os.path.split(ret[0])
                 os.startfile(oPath)
         else:
             self.args.works = int(self.args.works)
-            allFiles = azurlaneUnpack.list_all_files(self.args.directorie)
-            azurlaneUnpack.unpackTextureAllFutureP(allFiles,self.args.works)
+            allFiles = self.azurlaneUnpack.list_all_files(self.args.directorie)
+            self.azurlaneUnpack.unpackTextureAllFutureP(allFiles,self.args.works)
             outPath = os.path.join(CWD, "output",self.args.directorie)
             os.startfile(outPath)
         self.clearEtcPackTmp()
@@ -33,7 +34,11 @@ class AzurlaneUnpackControl:
         p.add_argument('-f', '--file', help="input a file path begin with com.bilibili.azurlane\n输入需要解包的文件目录,以com.bilibili.azurlane开头")
         p.add_argument('-d','--directorie',default="com.bilibili.azurlane\\files\\AssetBundles", help="input a directorie path begin with com.bilibili.azurlane, default com.bilibili.azurlane\\files\\AssetBundles \n输入需要解包的文件夹目录,以com.bilibili.azurlane开头, 默认为com.bilibili.azurlane\\files\\AssetBundles")
         p.add_argument('-w','--works',default=os.cpu_count(), help="max works to handdle files, default os.cpu_count()\n 处理解包的最大进程数,默认跑满CPU")
+        p.add_argument('-q','--quality',default=80, help="extract image quality, default 80\n 导出图片的质量,默认80")
         self.args = p.parse_args(args)
+        self.args.quality = int(self.args.quality)
+        if not 0<=self.args.quality<=100:
+            self.args.quality = 80
 
 if __name__ == '__main__':
 
@@ -42,5 +47,5 @@ if __name__ == '__main__':
     # if not ass:
     #     ass = ['-h']
     app = AzurlaneUnpackControl(ass)
-    app.clearEtcPackTmp()
-    # app.run()
+    # app.clearEtcPackTmp()
+    app.run()
